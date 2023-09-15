2da8d6f4b5ac619cf55505ccc8b8ea491ab39028
asatbekk <asatcom26@gmail.com>
2023-09-16 01:48:16 +0600

    devass1
    to github

---
+++ My project (1)/Assets/CubeRotation.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotation : MonoBehaviour
+{
+ public float rotationSpeed = 30f;
+ void Update()
+ {
+ // Rotate the cube around the y-axis
+ transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+ }
+}
+
